73d94062b18a3b16d4e4b0251385251991953744
Raffi Dary <rraffidh@gmail.com>
2025-05-23 20:25:42 +0700

    Finished authentication module

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+def index(request):
+    
+    if not request.user.is_authenticated:
+        return redirect('home')
+    
+    
+    user_role = request.session.get('user_role')
+    
+    
+    if user_role == 'klien_individu':
+        return klien_profile(request)
+    elif user_role == 'klien_perusahaan':
+        return kliencompany_profile(request)
+    elif user_role == 'front_desk':
+        return frontdesk_profile(request)
+    elif user_role == 'dokter_hewan':
+        return dokter_profile(request)
+    elif user_role == 'perawat_hewan':
+        return perawat_profile(request)
+    else:
+        return redirect('home')
 
 def dokter_profile(request):
     return render(request, 'dashboard/dokter.html')
