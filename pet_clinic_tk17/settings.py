5904a4ff61a071b1035689a358f4f75774d5758e
Raffi Dary <rraffidh@gmail.com>
2025-05-29 22:56:05 +0700

    Disabled Debug

--- pet_clinic_tk17/settings.py
+++ pet_clinic_tk17/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oh=7-0gl(oskq)bcb_%7c$^r21h$40$&0yj)qg#f0&x9e_r^#i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "tk17.vercel.app"]
 
