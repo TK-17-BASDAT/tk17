d45733dc7a4a8c39d7aa5714c9e1750fe406524e
claraaurelia <clarasetiady@gmail.com>
2025-05-29 23:03:58 +0700

    push bismillah

--- manajemen_vaksin/urls.py
+++ manajemen_vaksin/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'manajemen_vaksin'
 
 urlpatterns = [
-    path('vaksin/', views.vaccination_list_view, name='vaccination_list'),
+    path('vaksin/', views.VaccinationListView.as_view(), name='vaccination_list'),
     path('create/', views.vaccination_create_view, name='vaccination_create'),
     path('update/<uuid:kunjungan_id>/', views.vaccination_update_view, name='vaccination_update_view'),
     path('delete/<uuid:kunjungan_id>/', views.vaccination_delete_view, name='vaccination_delete'),
