98be695e17eac907008e6c34213826df1008fa99
Raffi Dary <rraffidh@gmail.com>
2025-05-29 20:55:54 +0700

    Deployement part 4 debugging

--- pet_clinic_tk17/settings.py
+++ pet_clinic_tk17/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oh=7-0gl(oskq)bcb_%7c$^r21h$40$&0yj)qg#f0&x9e_r^#i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
